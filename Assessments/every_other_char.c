6eadfb9acf293244214cff0216b16e4ce1297190
Hoang Phuc Luan Truong <83048295+truongluan303@users.noreply.github.com>
2021-11-13 22:56:44 -0800

    added on 11/13/2021

---
+++ Assessments/every_other_char.c
@@ -0,0 +1,41 @@
+/*
+Implement a C function that takes a null-terminated string and returns 
+a new heap-allocated null-terminated string containing every other 
+character from original string (e.g., “house” -> “hue”).
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+char* strdup_every_other_char(const char* s)
+{
+    int len = strlen(s);
+    char* result = (char*)malloc((len + 1) * sizeof(char));
+
+    int i = 0;
+    int j = 0;
+    while (i < len)
+    {
+        result[j] = s[i];
+        i += 2;
+        j += 1;
+    }
+    result[j] = '\0';
+
+    return result;
+}
+
+
+
+int main()
+{
+    char string1[] = "123456789";
+    char string2[] = "01234567";
+
+    printf(strdup_every_other_char(string1));
+    printf("\n");
+    printf(strdup_every_other_char(string2));
+
+    return 0;
+}
---
+++ Assessments/every_other_char_in_string.py
@@ -0,0 +1,24 @@
+'''
+Implement a Python function that takes a string and returns a string containing 
+every other character from original string (e.g., “house” -> “hue”).
+'''
+
+def remove_every_other_char(s: str) -> str:
+
+    result = ''
+
+    for i in range(0, len(s), 2):
+        result += s[i]
+
+    return result
+
+
+
+def main() -> None:
+    print(remove_every_other_char('house'))
+    print(remove_every_other_char('abcdef'))
+    print(remove_every_other_char('1.2.3.4.5.6.7.8.9'))
+
+
+if __name__ == "__main__":
+    main()
